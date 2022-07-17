21d070d0d78df16eed02349c4e9e9b12d2cd1bfe
Kelikume K.O <kelikume@gmail.com>
2022-07-17 22:48:37 +0100

    added solution folders

---
+++ InversePermutationSolution/Permutation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InversePermutationSolution
+{
+    public class Permutation
+    {               
+           public int[] InversePermutation(int[] arr, int size)
+            {
+                int i, j;
+                int[] result = new int[size];
+
+                for (i = 0; i < size; i++)
+                    {
+                        for (j = 0; j < size; j++)
+                        {                        
+                            if (arr[j] == i + 1)
+                            {
+                                int items  = j + 1;
+                                result[i] = items;                        
+                            }
+                        }
+                    }            
+                
+                return result; 
+            }
+    }
+}
+
+
---
+++ InversePermutationSolution/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InversePermutationSolution
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {   
+            //To test
+            Permutation per = new Permutation();    
+            int[] arr = { 1, 4, 3, 2 };
+            int size = 4;
+            var output = per.InversePermutation(arr, size);
+                        
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
